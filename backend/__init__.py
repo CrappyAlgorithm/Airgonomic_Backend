7ae3bf3e8c0b26b1352724fd27e66da5ac8884a2
raspberry <berry@sp.ifm>
2020-01-07 20:35:20 +0100

    added creation of user and assignments table to register

--- backend/__init__.py
+++ backend/__init__.py
@@ -29,4 +29,7 @@ def create_app(test_config=None):
     from . import db
     db.init_app(app)
 
+    from . import register
+    app.register_blueprint(register.bp)
+
     return app
--- backend/register.py
+++ backend/register.py
@@ -4,11 +4,11 @@ from backend.db import get_db
 
 bp = fl.Blueprint('register', __name__, url_prefix='/register')
 
-@bp.route('/',methods=('POST'))
+@bp.route('/',methods=['POST'])
 def register():
-    if request.method == 'POST':
-        username = request.args.get('username', '')
-        password = request.args.get('password', '')
+    if fl.request.method == 'POST':
+        username = fl.request.args.get('username', '')
+        password = fl.request.args.get('password', '')
         db = get_db()
         error = None
 
@@ -23,6 +23,30 @@ def register():
 
         if error is None:
             #insert user to db and generate link to all rooms/windows
-            #send success
-            pass
-        #send error
+            add_user(db, username, password)
+            return 'success'
+        return error
+
+def add_user(db, username, password):
+    cursor = db.cursor()
+    db.execute(
+        'INSERT INTO user (username, password, is_admin) VALUES (?,?,0)',
+        (username, password)
+    )
+    cursor.execute(
+        'SELECT id FROM user WHERE username = ?', (username,)
+    )
+    user_id = cursor.fetchone()[0] 
+    print(user_id)
+    for room in cursor.execute('SELECT id FROM room'):
+        db.execute(
+            'INSERT INTO assignment (alias, user_id, allowed, room_id) VALUES ("",?,0,?)',
+            (user_id, room[0])
+        )
+    for window in cursor.execute('SELECT id FROM window'):
+        db.execute(
+            'INSERT INTO assignment (alias, user_id, allowed, window_id) VALUES ("",?,0,?)',
+            (user_id, window[0])
+        )
+    db.commit()
+
