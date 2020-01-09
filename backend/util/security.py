67623a77fecbfe3e0a19d285483fb3db3c8e7157
raspberry <berry@sp.ifm>
2020-01-09 23:45:11 +0100

    Added util to get user and check rights. Implementation need to be changed in case of use a real token instead of user id as token.

---
+++ backend/util/security.py
@@ -0,0 +1,22 @@
+from flask import abort
+from backend.util.db import get_db() 
+from backend.util.response_code import *
+from backend.util.arg_parser import parse
+
+def get_user(token):
+    db = get_db()
+    if token is None:
+        abort(UNAUTHORIZED)
+    user_id = parse(token, 0, min=0)
+    if db.execute(
+        'SELECT id FROM user WHERE id = ?', (user_id,)
+    ).fetchone() is None:
+        abort(BAD_REQUEST)
+
+def is_admin(token):
+    user_id = get_user(token)
+    if db.execute(
+        'SELECT id FROM user WHERE id = ? AND is_admin = 1', (user_id,)
+    ).fetchone() is None:
+        abort(FORBIDDEN)
+    return user_id
