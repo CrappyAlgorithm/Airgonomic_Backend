07de573be057bf06c8d587db0b1e63194464fb4c
raspberry <berry@sp.ifm>
2020-01-09 09:52:01 +0100

    Added PUT request to configuration resource

--- backend/configuration.py
+++ backend/configuration.py
@@ -6,7 +6,7 @@ from backend.db import get_db
 
 bp = Blueprint('configuration', __name__, url_prefix='/configuration')
 
-@bp.route('/',methods=['GET'])
+@bp.route('/',methods=['GET', 'PUT'])
 def configuration():
     token = request.args.get('token', '')
     db = get_db()
@@ -22,6 +22,16 @@ def configuration():
     if request.method == 'GET':
         return Response(generate_view(db), status=rc.OK, mimetype='application/json')
 
+    if request.method == 'PUT':
+        try:
+            co2 = int(request.args.get('co2', -1))
+            humidity = float(request.args.get('humidity', -1.0))
+            automatic_enable = int(request.args.get('automatic_enable', -1))
+        catch ValueError:
+            return Response('Error while parsing parameter.', status=rc.BAD_REQUEST)
+        set_configuration(db, co2, huidity, automatic_enable)
+        return Response('', status=rc.OK)
+
 def generate_view(db):
     cursor = db.cursor()
     ret = {}
@@ -32,3 +42,24 @@ def generate_view(db):
         ret['humidity'] = cur[1]
         ret['automatic_enable'] = cur[2]
     return json.dumps(ret)
+
+def set_configuration(db, co2, humidity, automatic_enable):
+    if co2 > 0:
+        db.execute(
+            'UPDATE configuration SET co2 = ? ' +
+            'WHERE id = 1',
+            (co2,)
+        )
+    if humidity > 0.0:
+        db.execute(
+            'UPDATE configuration SET humidity = ? ' +
+            'WHERE id = 1',
+            (humidity,)
+        )
+    if automatic_enable >= 0 and automatic_enable < 2:
+        db.execute(
+            'UPDATE configuration SET automatic_enable = ? ' +
+            'WHERE id = 1',
+            (automatic_enable,)
+        )
+    db.commit()
