b92e3d03701c5e9a9ffd43defd71e62d62f6674d
raspberry <berry@sp.ifm>
2020-01-09 10:58:37 +0100

    Added configuration resource

--- backend/__init__.py
+++ backend/__init__.py
@@ -41,4 +41,7 @@ def create_app(test_config=None):
     from . import user
     app.register_blueprint(user.bp)
 
+    from . import configuration
+    app.register_blueprint(configuration.bp) 
+
     return app
