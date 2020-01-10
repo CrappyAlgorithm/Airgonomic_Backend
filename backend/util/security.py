183f44e96f6c127354b072f7c75a6a96e42e6556
raspberry <berry@sp.ifm>
2020-01-10 18:49:50 +0100

    Added function to check user rights for room and window

--- backend/util/security.py
+++ backend/util/security.py
@@ -22,3 +22,31 @@ def check_admin(token):
     ).fetchone() is None:
         abort(FORBIDDEN)
     return user_id
+
+def check_user_room(token, room_id):
+    db = get_db()
+    user_id = get_user(token)
+    if db.execute(
+        'SELECT id FROM user WHERE id = ? AND is_admin = 1', (user_id,)
+    ).fetchone() is None:
+        if db.execute(
+            'SELECT room.id ' +
+            'FROM room join assignment ' +
+            'ON room.id = assignment.room_id ' +
+            'WHERE assignment.user_id = ? ' +
+            'AND assignment.allowed = 1',
+            (user_id,)
+        ).fetchone() is None:
+            abort(400)
+    return user_id
+
+def check_user_window(token, window_id):
+    db = get_db()
+    room_id = db.execute(
+        'SELECT room.id ' +
+        'FROM room join window ' +
+        'ON room.id = window.room_id ' +
+        'WHERE window.id = ?',
+        (window_id,)
+    ).fetchone()
+    return check_user_room(token, room_id)
