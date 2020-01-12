d98c8f601fd0e66abe8c5c0428b50d3c0d9099ff
raspberry <berry@sp.ifm>
2020-01-12 01:31:22 +0100

    Added resource window/control with PUT and POST.

--- backend/resources/window.py
+++ backend/resources/window.py
@@ -4,12 +4,12 @@ from flask import (Blueprint, Response, request)
 from backend.util.response_code import *
 from backend.util.db import get_db
 from backend.util.arg_parser import parse
-from backend.util.security import check_user_window, get_user
+from backend.util.security import check_user_window, get_user, get_room
 
 bp = Blueprint('window', __name__, url_prefix='/window')
 
 @bp.route('',methods=['PUT'])
-def user():
+def window():
     db = get_db()
 
     if request.method == 'PUT':
@@ -26,11 +26,31 @@ def user():
             user_id = check_user_window(request.args.get('token', None), window_id)
         else:
             user_id = get_user(request.args.get('token', None))
-        set_values(db, user_id, window_id, alias, is_open, automatic_enable)
+        set_values(db, window_id, user_id, alias, is_open, automatic_enable)
         return Response('', status=OK)
 
-def set_values(db, user_id, window_id, alias, is_open, automatic_enable):
-    if alias is not None:
+@bp.route('/control/',methods=['PUT', 'POST'])
+def window_control():
+    db = get_db()
+    room_id = get_room(request.args.get('token', None))
+
+    if request.method == 'PUT':
+        window_id = parse(request.args.get('id', None), 0, min=1)
+        is_open = parse(request.args.get('is_open', None), 0, min=0, max=1)
+        automatic_enable = parse(request.args.get('automatic_enable', None), 0, min=0, max=1)
+        if db.execute(
+            'SELECT id FROM window WHERE id = ? and room_id = ?',
+            (window_id, room_id)
+        ).fetchone() is None:
+            return Response('No valid window id given', status=BAD_REQUEST)
+        set_values(db, window_id, is_open=is_open)
+        return Response('', status=OK)
+
+    if request.method == 'POST':
+        return Response(create_window(db, room_id), status=CREATED, mimetype='application/json')
+
+def set_values(db, window_id, user_id=None, alias=None, is_open=None, automatic_enable=None):
+    if alias is not None and user_id is not None:
         db.execute(
             'UPDATE assignment SET alias = ? ' +
             'WHERE user_id = ? ' +
@@ -50,3 +70,27 @@ def set_values(db, user_id, window_id, alias, is_open, automatic_enable):
             (automatic_enable, window_id)
         )
     db.commit()
+
+def create_window(db, room_id):
+    cursor = db.cursor()
+    db.execute(
+        'INSERT INTO window (room_id, is_open, automatic_enable) ' +
+        'VALUES (?,0,0)',
+        (room_id,)
+    )
+    id = db.execute(
+        'SELECT max(id) FROM window ' + 
+        'WHERE room_id = ?',
+        (room_id,)
+    ).fetchone()[0]
+
+    for user in cursor.execute('SELECT id FROM user'):
+        db.execute(
+            'INSERT INTO assignment (alias, user_id, allowed, window_id) VALUES ("",?,0,?)',
+            (user[0], id)
+        )
+
+    ret = {}
+    ret['id'] = id
+    db.commit()
+    return json.dumps(ret)
