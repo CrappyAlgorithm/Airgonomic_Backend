1682a0b519deb8ecea64b88de8e01024041b84b2
raspberry <berry@sp.ifm>
2020-01-20 21:02:11 +0100

    Added loger configuration

--- control/__init__.py
+++ control/__init__.py
@@ -1,8 +1,10 @@
 from time import sleep
 from configuration_handler import load_configuration
 from room import Room
+import logging
 
 def run():
+    logging.basicConfig(filename='control.log', format='%(asctime)s-%(process)d-%(message)s', level=logging.INFO)
     sleep_duration, room = load_configuration()
     while True:
         room.get_update()
