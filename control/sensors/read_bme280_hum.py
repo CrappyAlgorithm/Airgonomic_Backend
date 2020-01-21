c79a5d264b8a5571bf7124720b1cad2cff6491ca
raspberry <berry@sp.ifm>
2020-01-21 16:50:58 +0100

    Added script to read the bme280 humidity sensor

---
+++ control/sensors/read_bme280_hum.py
@@ -0,0 +1,11 @@
+import bme280
+import smbus2
+
+port = 1
+address = 0x77
+bus = smbus2.SMBus(port)
+bme280.load_calibration_params(bus,address)
+
+def read_humidity:
+    bme280_data = bme280.sample(bus,address)
+    return bme280_data.humidity
